61d2cfb90f4318de87cc24a03e92c4f7400005e8
Aaron <zoto724@gmail.com>
2014-05-09 12:42:44 -0400

    added camera pull script This pulls camera images every half second and dates them

---
+++ camerapull_safe.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+
+#you must change the username and password below before this will work
+while :
+do
+ for IP in 192.168.1.10 192.168.1.11 192.168.1.13 192.168.1.15
+  do
+    curl -u username:password $IP/snapshot.cgi > camera.$IP.$(date +%Y-%m-%d_%H:%M:%S:%2N).jpg & 
+ done
+while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
+sleep .5
+done
+
