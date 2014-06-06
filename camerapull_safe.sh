f2adb1014f1a3d6d254504fc6bde9233db8afbfe
zoto724 <zoto724@gmail.com>
2014-06-06 10:05:46 -0400

    Update camerapull_safe.sh

--- camerapull_safe.sh
+++ camerapull_safe.sh
@@ -1,14 +1,13 @@
-#/bin/bash
-
-
 #you must change the username and password below before this will work
 while :
 do
  for IP in 192.168.1.10 192.168.1.11 192.168.1.13 192.168.1.15
   do
-    curl -u username:password $IP/snapshot.cgi > camera.$IP.$(date +%Y-%m-%d_%H:%M:%S:%2N).jpg & 
- done
+    curl --silent -u user:password $IP/snapshot.cgi >camera.$IP.$(date +%Y-%m-%d_%H:%M:%S:%2N).jpg &
+    #curl --silent -u user:password $IP/snapshot.cgi >/dev/null &
+    #echo "$IP  $(date +%Y-%m-%d_%H:%M:%S:%2N)" >> timestamps
+    #this code will test how fast we can pull images without disk IO overhead.  
+  done
  wait
-sleep .5
+#sleep .25
 done
-
