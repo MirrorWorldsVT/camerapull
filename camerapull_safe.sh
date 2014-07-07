436e4ea85e522e9b613974c2b9f1bf9c849302c1
Edward Coe (EdVentureTime) <eoccoe@vt.edu>
2014-07-07 12:36:26 -0400

    Updated IP of problematic camera

--- camerapull_safe.sh
+++ camerapull_safe.sh
@@ -1,7 +1,7 @@
 #you must change the username and password below before this will work
 while :
 do
- for IP in 192.168.1.10 192.168.1.11 192.168.1.13 192.168.1.15
+ for IP in 192.168.1.10 192.168.1.11 192.168.1.12 192.168.1.15
   do
     curl --silent -u user:password $IP/snapshot.cgi >camera.$IP.$(date +%Y-%m-%d_%H:%M:%S:%2N).jpg &
     #curl --silent -u user:password $IP/snapshot.cgi >/dev/null &
